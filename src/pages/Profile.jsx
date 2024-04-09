40b2ee439c905a1c0d638bae5c4e5432bb39010d
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-04-09 07:48:02 +0200

    authentication to firbase successfully

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react"
 
-
+import { getAuth } from "firebase/auth"
 function Profile() {
+  const auth = getAuth()
+
+  useEffect(() => {
+    console.log("Profile")
+    console.log(auth.currentUser)
+  },[]);
+
   return (
     <div>
       <h1>Profile</h1>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 import ArrowRightIcon from "../assets/svg/svg-to-component/ArrowRightIcon";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -18,7 +21,26 @@ function SignIn() {
       ...prevState,
       [e.target.id]: e.target.value,
     }));
+  };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -27,47 +49,46 @@ function SignIn() {
         <header>
           <p className="pageHeader">Welcome Back!</p>
         </header>
-        <form>
+        <form onSubmit={onSubmit}>
           <input
-            type='email'
-            className='emailInput'
-            placeholder='Email'
-            id='email'
+            type="email"
+            className="emailInput"
+            placeholder="Email"
+            id="email"
             value={email}
             onChange={onChange}
           />
-          <div className='passwordInputDiv'>
+          <div className="passwordInputDiv">
             <input
-              type={showPassword ? 'text' : 'password'}
-              className='passwordInput'
-              placeholder='Password'
-              id='password'
+              type={showPassword ? "text" : "password"}
+              className="passwordInput"
+              placeholder="Password"
+              id="password"
               value={password}
               onChange={onChange}
             />
 
             <img
               src={visibilityIcon}
-              alt='show password'
-              className='showPassword'
+              alt="show password"
+              className="showPassword"
               onClick={() => setShowPassword((prevState) => !prevState)}
             />
           </div>
-          <Link to='/forgot-password' className="forgotPasswordLink">
+          <Link to="/forgot-password" className="forgotPasswordLink">
             Forgot Password
           </Link>
-          <div className='signInBar'>
-            <p className='signInText'>Sign In</p>
-            <button className='signInButton'>
-              <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
+          <div className="signInBar">
+            <p className="signInText">Sign In</p>
+            <button className="signInButton">
+              <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
             </button>
           </div>
         </form>
 
-
         {/* Google OAuth */}
 
-        <Link to='/sign-up' className='registerLink'>
+        <Link to="/sign-up" className="registerLink">
           Sign Up Instead
         </Link>
       </div>

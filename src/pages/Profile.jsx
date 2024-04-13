295bc10e9bc67a831a6a48388756bd6cf0a0b3bd
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-04-13 04:12:37 +0200

    Start Create Listings Page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 import { getAuth, updateProfile } from "firebase/auth";
 import { updateDoc, doc } from "firebase/firestore";
 import { db } from "../firebase.config";
 
 import { toast } from "react-toastify";
+
+import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg"
+import homeIcon from "../assets/svg/homeIcon.svg"
+import CreateListing from "./CreateListing";
 
 function Profile() {
   const auth = getAuth();
@@ -93,6 +97,11 @@ function Profile() {
             onChange={onChange}
           />
         </div>
+        <Link to={"/create-listing"} className="createListing">
+            <img src={homeIcon} alt="home" />
+            <p>Sell or rent your home</p>
+            <img src={arrowRight} alt="arrow right" />
+        </Link>
       </main>
     </div>
   );
